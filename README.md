dd26ed3d0c56d1f298a7a78a698006794db6fe1c
neiranie <email_anda@contoh.com>
2026-03-13 02:22:36 +0700

    Update

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-# Tugas 1: CRUD KTP - Spring Boot & MySQL
+# Tugas 1: Membuat Program di Server dengan API untuk CRUD KTP menggunakan Spring Boot dan MySQL
 
-### 1. Database MySQL (Schema spring)
+### 1. Database MySQL 
 ![Database](./Mysql_Database_Tugas1.png)
 
-### 2. Struktur Folder (Project Architecture)
+### 2. Struktur Folder 
 ![Project Tree](./Project_Tree_Tugas1.png)
 
-### 3. Hasil Browser (GET Semua Data)
+### 3. Hasil Browser 
 ![Hasil Browser](./Tampilan_Browser_Tugas1.png)
 
-### 4. Error Handling (Unique Constraint)
+### 4. Error Handling 
 ![Error Handling](./Error_Handling_Tugas1.png)
+
+# Tugas 2: Membuat Program Client-side menggunakan HTML, CSS, JavaScript, dan Ajax JQuery
+### 1. Create
+![Create](./Create_Tugas2.png)
+
+### 2. Read
+![Read](./TampilanWeb_Tugas2.png)
+![Read](./Read_Tugas2.png)
+
+### 3. Update
+![Update](./Update_Tugas2.png)
+![Update](./PopUp_Update_Tugas2.png)
+
+### 4. Delete
+![Delete](./Delete_Tugas2.png)
--- src/main/java/com/example/demo/controller/KtpController.java
+++ src/main/java/com/example/demo/controller/KtpController.java
@@ -8,32 +8,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ktp")
+@CrossOrigin(origins = "*")
 public class KtpController {
+
     @Autowired
     private KtpService ktpService;
 
-    @PostMapping
-    public Ktp createKtp(@RequestBody Ktp ktp) {
-        return ktpService.saveKtp(ktp);
-    }
-
     @GetMapping
-    public List<Ktp> getAllKtp() {
+    public List<Ktp> getAll() {
         return ktpService.getAllKtp();
     }
 
-    @GetMapping("/{id}")
-    public Ktp getKtpById(@PathVariable Integer id) {
-        return ktpService.getKtpById(id);
+    @PostMapping
+    public Ktp create(@RequestBody Ktp ktp) {
+        return ktpService.saveKtp(ktp);
     }
 
     @PutMapping("/{id}")
-    public Ktp updateKtp(@PathVariable Integer id, @RequestBody Ktp ktp) {
+    public Ktp update(@PathVariable Integer id, @RequestBody Ktp ktp) {
         return ktpService.updateKtp(id, ktp);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteKtp(@PathVariable Integer id) {
+    public void delete(@PathVariable Integer id) {
         ktpService.deleteKtp(id);
     }
 }
