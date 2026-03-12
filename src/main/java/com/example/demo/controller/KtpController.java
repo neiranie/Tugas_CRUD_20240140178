b6019a8e8d9cb443066e181d6419da60b1e85fa5
neiranie <email_anda@contoh.com>
2026-03-12 23:36:35 +0700

    Adding Ktp Controller

---
+++ src/main/java/com/example/demo/controller/KtpController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Ktp;
+import com.example.demo.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ktp")
+public class KtpController {
+    @Autowired
+    private KtpService ktpService;
+
+    @PostMapping
+    public Ktp createKtp(@RequestBody Ktp ktp) {
+        return ktpService.saveKtp(ktp);
+    }
+
+    @GetMapping
+    public List<Ktp> getAllKtp() {
+        return ktpService.getAllKtp();
+    }
+
+    @GetMapping("/{id}")
+    public Ktp getKtpById(@PathVariable Integer id) {
+        return ktpService.getKtpById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Ktp updateKtp(@PathVariable Integer id, @RequestBody Ktp ktp) {
+        return ktpService.updateKtp(id, ktp);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteKtp(@PathVariable Integer id) {
+        ktpService.deleteKtp(id);
+    }
+}
